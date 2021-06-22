fcbde6a07b1a7339958a3ff8e3e998ffcb60adc6
GabrielFSSantos <gabrielfelipesouzasantos@gmail.com>
2021-06-22 13:12:38 -0300

    Home page layout using Sass 5.0 (scss)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import { Button } from './components/Button';
+import { Home } from './pages/Home';
 
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-      <Button>{0}</Button>
-      <Button/>
-    </>
+    <Home />
   );
 }
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import illustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg'
+import googleIconImg from '../assets/images/google-icon.svg'
+
+import '../styles/auth.scss';
+
+export function Home() {
+  
+
+  return (
+    <div id="page-auth" >
+      <aside>
+        <img src={illustrationImg} alt="illustration" />
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
+        <p>Tire dúvidas da sua audiência em tempo-real</p>
+      </aside>
+      <main>
+        <div className="main-content" >
+          <img src={logoImg} alt="Letmeask" />
+          <button className="create-room" >
+            <img src={googleIconImg} alt="Google" />
+            Crie sua sala com Google
+          </button>
+          <div className="separator" >ou entre em uma sala</div>
+          <form>
+            <input type="text" placeholder="Digite o código da sala" />
+            <button type="submit">Entrar na sala</button>
+          </form>
+        </div>
+      </main>
+    </div>
+  )
+}
