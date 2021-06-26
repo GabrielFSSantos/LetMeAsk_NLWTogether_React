ce0d862faae30b8010dc84b599608537f41346fa
GabrielFSSantos <gabrielfelipesouzasantos@gmail.com>
2021-06-26 09:46:23 -0300

    Layout and Styles Room

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import { Home } from './pages/Home';
 import { NewRoom } from './pages/NewRoom';
 import { Room } from './pages/Room';
+
 import { AuthContextProvider } from './contexts/AuthContext';
 
 function App() {
@@ -10,7 +12,7 @@ function App() {
       <AuthContextProvider>
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/rooms/new" exact component={NewRoom} />
+          <Route path="/rooms/new" component={NewRoom} />
           <Route path="/rooms/:id" component={Room} />
         </Switch>
       </AuthContextProvider>
--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,11 +1,34 @@
+import { Button } from '../components/Button';
 
+import logoImg from '../assets/images/logo.svg';
+
+import '../styles/room.scss';
 
 export function Room() {
-  
-
   return (
-    <div id="page-auth" >
-      <h1>Sala</h1>
+    <div id="page-room" >
+      <header>
+        <div className="content">
+          <img src={logoImg} alt="Letmeask" />
+          <div>codigo</div>
+        </div>
+      </header>
+
+      <main>
+        <div className="room-title">
+          <h1>Sala React</h1>
+          <span>4 perguntas</span>
+        </div>
+
+        <form>
+          <textarea placeholder="O que você quer perguntar?" />
+          
+          <div className="form-footer">
+            <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
+            <Button type="submit">Enviar pergunta</Button>
+          </div>
+        </form>
+      </main>
     </div>
   );
 }
